175025c1dffa0185d5cd60cc51272029971898b0
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2025-02-19 08:05:59 +0500

    understood the concept of combineReducers

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-import { createStore } from "redux"
+import { combineReducers, createStore } from "redux"
 
 // Step 01:
 // Creating a store: 
@@ -58,14 +58,69 @@ import { createStore } from "redux"
 // Let's create another reducer in order to just show the bank balance
 
 
-const showBalance = (state = 45600 , {type , payload}) => {
-    if (type == "showBalance") {
-        return state
+// const showBalance = (state = 45600 , {type , payload}) => {
+//     if (type == "showBalance") {
+//         return state
+//     }
+// }
+
+// const backStore = createStore(showBalance)
+
+// backStore.dispatch({type: "showBalance"})
+
+// console.log(backStore.getState())
+
+// 19-Feb-2025 
+// Let's Learn about merge reducers
+
+const cashDeposit = (state = 100 , {type , payload}) => {
+    if (type == "cashDeposit") {
+        return state + payload
     }
+    return state
 }
 
-const backStore = createStore(showBalance)
+const withdrawMoney = (state = 50 , {type , payload}) => {
+    if (type == "withdrawMoney") {
+        if (state < payload) {
+            return "Sorry , you have lesser amount in bank ..."
+        }
+        else {
+            return state - payload
+        }
+    }
+    return state
+}
 
-backStore.dispatch({type: "showBalance"})
+const checkBalance= (state = 200 , {type}) => {
+    
+    if (type == "checkBalance") {
+        return state
+    }
+    return state
+}   
 
-console.log(backStore.getState())
+
+// Now let's call all these reducers separately 
+
+// const bankStore = createStore(withdrawMoney)
+
+// bankStore.dispatch({type: "withdrawMoney" , payload: 50})
+
+// console.log(bankStore.getState())
+
+// Now , If I want to combinely call all of them: 
+
+const mergeReducers = combineReducers({
+    BalanceCheck : checkBalance , 
+    deposit: cashDeposit,
+    withdraw: withdrawMoney
+})
+
+const bankStore = createStore(mergeReducers)
+
+bankStore.dispatch({type:"cashDeposit" , payload: 500})
+bankStore.dispatch({type:"withdrawMoney" , payload: 40})
+bankStore.dispatch({type: "checkBalance"})
+
+console.log(bankStore.getState())
